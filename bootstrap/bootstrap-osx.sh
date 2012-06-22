fd325cfcc7662ea14b35fada4f98e9d2c12829dd
Ben O'Hara <bohara@gmail.com>
2012-06-23 01:51:40 +1000

    adding bootstrap, hgrc and muttrc

---
+++ bootstrap/bootstrap-osx.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+chsh -s /bin/zsh
+sudo port install coreutils +with_default_names
+sudo port install screen
+sudo port install links
+sudo port install mutt-devel +sasl +ssl +smtp +sidebar +headercache +tokyocabinet +gpgme
+sudo port install irssi
+sudo port install proctools
+sudo port install mtr
+sudo port install wget
+sudo port install tmux
+sudo port install tmux-pasteboard
+sudo port install gnupg
+sudo port install ipcalc
