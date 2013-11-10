d5a807ede2b6907b44f5bbe5efa51cb8c23b9bd4
Ben O'Hara <bohara@gmail.com>
2013-11-10 18:33:43 +1000

    fix

--- tmux/tmuxifier/layouts/bgplab-configs.window.sh
+++ tmux/tmuxifier/layouts/bgplab-configs.window.sh
@@ -8,14 +8,14 @@ new_window "bgplab-configs"
 
 # Split window into panes.
 #split_v 20
-split_h 30
+split_v 75
 
 # Run commands.
-run_cmd "vi" 1     # runs in pane 1
+run_cmd "vi" 2     # runs in pane 1
 
 # Paste text
 #send_keys "top"    # paste into active pane
 #send_keys "date" 1 # paste into active pane
 
 # Set active pane.
-select_pane 1
+select_pane 2
--- tmux/tmuxifier/layouts/puppet-configs.window.sh
+++ tmux/tmuxifier/layouts/puppet-configs.window.sh
@@ -8,15 +8,15 @@ new_window "puppet-configs"
 
 # Split window into panes.
 #split_v 20
-split_h 30
+split_v 75
 
 # Run commands.
-run_cmd "vi" 1     # runs in pane 1
-run_cmd "vagrant status" 2  # runs in pane 2
+run_cmd "vagrant status" 1  # runs in pane 1
+run_cmd "vi" 2     # runs in pane 2
 
 # Paste text
 #send_keys "top"    # paste into active pane
 #send_keys "date" 1 # paste into active pane
 
 # Set active pane.
-select_pane 1
+select_pane 2
